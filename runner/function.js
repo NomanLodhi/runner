f019b0f72e833cb2c42658bb603cb596e1a69d39
NomanLodhi <110075584+NomanLodhi@users.noreply.github.com>
2023-10-23 08:24:42 -0700

    Add files via upload

---
+++ runner/function.js
@@ -0,0 +1,98 @@
+function Run1(){
+    document.getElementById('pos1').style.display='block'
+    document.getElementById('pos2').style.display='none'
+    // document.getElementById('pos1').style.display='block'
+
+}
+setInterval(Run1,1000)
+function Run2(){
+    document.getElementById('pos2').style.display='block'
+    document.getElementById('pos1').style.display='none'
+    // document.getElementById('pos3').style.display='none'
+
+}
+setInterval(Run2,1500)
+function Run3(){
+    document.getElementById('pos2').style.display='none'
+    document.getElementById('pos1').style.display='none'
+    document.getElementById('pos3').style.display='block'
+}
+
+setInterval(Run3,2000)
+function Run4(){
+    document.getElementById('pos2').style.display='none'
+    document.getElementById('pos1').style.display='none'
+    document.getElementById('pos3').style.display='none'
+    document.getElementById('pos4').style.display='block'
+}
+
+setInterval(Run4,2500)
+function Run5(){
+    document.getElementById('pos2').style.display='none'
+    document.getElementById('pos1').style.display='none'
+    document.getElementById('pos3').style.display='none'
+    document.getElementById('pos4').style.display='none'
+    document.getElementById('pos5').style.display='block'
+}
+
+setInterval(Run5,3000)
+function Run6(){
+    document.getElementById('pos2').style.display='none'
+    document.getElementById('pos1').style.display='none'
+    document.getElementById('pos3').style.display='none'
+    document.getElementById('pos4').style.display='none'
+    document.getElementById('pos5').style.display='none'
+    document.getElementById('pos6').style.display='block'
+}
+
+setInterval(Run6,3500)
+function Run7(){
+    document.getElementById('pos2').style.display='none'
+    document.getElementById('pos1').style.display='none'
+    document.getElementById('pos3').style.display='none'
+    document.getElementById('pos4').style.display='none'
+    document.getElementById('pos5').style.display='none'
+    document.getElementById('pos6').style.display='none'
+    document.getElementById('pos7').style.display='block'
+}
+
+setInterval(Run7,4000)
+function Run8(){
+    document.getElementById('pos2').style.display='none'
+    document.getElementById('pos1').style.display='none'
+    document.getElementById('pos3').style.display='none'
+    document.getElementById('pos4').style.display='none'
+    document.getElementById('pos5').style.display='none'
+    document.getElementById('pos6').style.display='none'
+    document.getElementById('pos7').style.display='none'
+    document.getElementById('pos8').style.display='block'
+}
+
+setInterval(Run8,4500)
+function Run9(){
+    document.getElementById('pos2').style.display='none'
+    document.getElementById('pos1').style.display='none'
+    document.getElementById('pos3').style.display='none'
+    document.getElementById('pos4').style.display='none'
+    document.getElementById('pos5').style.display='none'
+    document.getElementById('pos6').style.display='none'
+    document.getElementById('pos7').style.display='none'
+    document.getElementById('pos8').style.display='none'
+    document.getElementById('pos9').style.display='block'
+}
+
+setInterval(Run9,5000)
+function Run10(){
+    document.getElementById('pos2').style.display='none'
+    document.getElementById('pos1').style.display='none'
+    document.getElementById('pos3').style.display='none'
+    document.getElementById('pos4').style.display='none'
+    document.getElementById('pos5').style.display='none'
+    document.getElementById('pos6').style.display='none'
+    document.getElementById('pos7').style.display='none'
+    document.getElementById('pos8').style.display='none'
+    document.getElementById('pos9').style.display='none'
+    document.getElementById('pos10').style.display='block'
+}
+
+setInterval(Run10,5500)
